0a029763182b50943940691ca4389cd1cc0064d1
Bhumika Koirala <koiralabhumika07@gmail.com>
2024-01-19 13:09:24 +1100

    Added col lastName for User Entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -45,6 +45,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, TwoFact
      */
     private $firstName;
 
+     /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups("user:read")
+     */
+    private $lastName;
+
     /**
      * @ORM\Column(type="string", length=255)
      */
